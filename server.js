b98b4d3d46d0457ecaeb5115ca48e8efaa637fc8
lrodenyoder <rodenlau000@gmail.com>
2022-03-03 19:30:40 -0600

    11.3.7

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming json data
 app.use(express.json());
+//middleware to access frontend files w/o having to write routes for each one
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -99,6 +101,23 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//should always come last! catch all for routes that don't exist
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
